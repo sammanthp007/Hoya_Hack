65c076d53ef2fe538bc3e578354d023afb503c8f
sammanthp007 <samman.thapa@bison.howard.edu>
2016-01-30 22:14:51 -0500

    angularjs: add, learn ng-app, ng-init, ng-repeat, ng-model

--- flask/proving_grounds.py
+++ flask/proving_grounds.py
@@ -1,9 +1,9 @@
-from twitter import *
-access_token = '2750763448-o3z1B8QE09iHGXKb9YhGZKZiOoaADrUTri8cVD0'
-access_token_secret = 'ghkX4dTPa98U3UsfuQgG6ASxGkBonPDa0ACUdjXkeKGTx'
-consumer_key = 'pTQqksnWdlZac2oVz6zOnK4qJ'
-consumer_key_secret = 'M29Ay2pM2nW6JlJWJINswpFVorINmHxX3FwWhYegPpJ9jpqB3D'
-data = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_key_secret))
+# from twitter import *
+# access_token = '2750763448-o3z1B8QE09iHGXKb9YhGZKZiOoaADrUTri8cVD0'
+# access_token_secret = 'ghkX4dTPa98U3UsfuQgG6ASxGkBonPDa0ACUdjXkeKGTx'
+# consumer_key = 'pTQqksnWdlZac2oVz6zOnK4qJ'
+# consumer_key_secret = 'M29Ay2pM2nW6JlJWJINswpFVorINmHxX3FwWhYegPpJ9jpqB3D'
+# data = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_key_secret))
 
 # Get your "home" timeline
 # a = give_me.statuses.home_timeline()
@@ -21,3 +21,8 @@ def get_dic_of_friends():
 		description = friend['description']
 		dic[username] = description
 	return dic
+
+# from yanker import yanker
+
+# a = yanker.Video('https://www.youtube.com/watch?v=i9MHigUZKEM')
+# b = a.download()
